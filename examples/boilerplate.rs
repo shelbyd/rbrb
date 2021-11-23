2f25c7bf03da66a1b532cdd90c85061af7f26ff0
Shelby Doolittle <shelby@shelbyd.com>
2021-11-23 13:16:12 -0500

    run only local player

---
+++ examples/boilerplate.rs
@@ -0,0 +1,91 @@
+use rbrb::{PlayerId, PlayerInputs, Request, SessionBuilder};
+
+use macroquad::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::{collections::BTreeMap, net::SocketAddr, ops::ControlFlow, time::Duration};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(long, default_value = "7000")]
+    local_port: u16,
+
+    #[structopt(long)]
+    local_index: PlayerId,
+
+    remote_players: Vec<SocketAddr>,
+}
+
+#[derive(Default, Serialize, Deserialize)]
+struct GameState {
+    value: isize,
+    player_cooldowns: BTreeMap<PlayerId, Duration>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Action {
+    Increment,
+    Decrement,
+}
+
+#[macroquad::main("Basic RbRb")]
+async fn main() {
+    let options = Options::from_args();
+
+    let mut session = SessionBuilder::default()
+        .remote_players(&options.remote_players)
+        .local_player(options.local_index, options.local_port)
+        .step_size(Duration::from_millis(16))
+        .start()
+        .unwrap();
+
+    let mut game_state = GameState::default();
+    loop {
+        while let ControlFlow::Continue(()) = session.next_request(|event| match event {
+            Request::SaveTo(vec) => bincode::serialize_into(vec, &game_state).unwrap(),
+            Request::CaptureLocalInput(vec) => {
+                let input = if is_key_down(KeyCode::Up) {
+                    Some(Action::Increment)
+                } else if is_key_down(KeyCode::Down) {
+                    Some(Action::Decrement)
+                } else {
+                    None
+                };
+                bincode::serialize_into(vec, &input).unwrap();
+            }
+            Request::Advance(dt, inputs) => {
+                game_state.player_cooldowns = game_state
+                    .player_cooldowns
+                    .iter()
+                    .filter_map(|(k, v)| if *v < dt { None } else { Some((*k, *v - dt)) })
+                    .collect();
+
+                let inputs: PlayerInputs<Option<Action>> =
+                    inputs.map(|vec| bincode::deserialize_from(&vec[..]).unwrap());
+                for (player_id, action) in inputs.iter() {
+                    let on_cooldown = game_state.player_cooldowns.contains_key(&player_id);
+                    if on_cooldown {
+                        continue;
+                    }
+
+                    if let Some(action) = action {
+                        match action {
+                            Action::Increment => game_state.value += 1,
+                            Action::Decrement => game_state.value -= 1,
+                        }
+                        game_state
+                            .player_cooldowns
+                            .insert(*player_id, Duration::from_millis(300));
+                    }
+                }
+            }
+            unhandled => {
+                panic!("unhandled request: {:?}", unhandled);
+            }
+        }) {}
+
+        draw_text(&format!("{}", game_state.value), 20.0, 20.0, 20.0, WHITE);
+
+        next_frame().await;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -37,3 +37,137 @@
 //!
 //! - [ ] Input delta encoding
 //! - [ ] Hub and spoke network
+
+use std::{
+    collections::{BTreeMap, HashMap},
+    net::SocketAddr,
+    ops::ControlFlow,
+    time::{Duration, Instant},
+};
+
+pub type SerializedState = Vec<u8>;
+pub type SerializedInput = Vec<u8>;
+pub type SimulationInstant = Duration;
+pub type PlayerId = u16;
+
+pub struct Session {
+    saved_states: BTreeMap<SimulationInstant, SerializedState>,
+    saved_inputs: BTreeMap<SimulationInstant, PlayerInputs>,
+
+    step_size: Duration,
+    local_index: PlayerId,
+
+    started_at: Instant,
+    host_at: Option<SimulationInstant>,
+}
+
+impl Session {
+    pub fn next_request<R>(
+        &mut self,
+        handler: impl FnOnce(Request) -> R,
+    ) -> ControlFlow<Option<R>, R> {
+        let host_at = match self.host_at {
+            Some(h) => h,
+            None => {
+                let mut state = Vec::new();
+                let ret = handler(Request::SaveTo(&mut state));
+                self.host_at = Some(Duration::ZERO);
+                self.saved_states.insert(Duration::ZERO, state);
+                return ControlFlow::Continue(ret);
+            }
+        };
+
+        if self.started_at.elapsed() > (host_at + self.step_size) {
+            let inputs = match self.inputs(host_at) {
+                Some(i) => i,
+                None => {
+                    let mut input = Vec::new();
+                    let ret = handler(Request::CaptureLocalInput(&mut input));
+                    self.saved_inputs
+                        .insert(host_at, PlayerInputs::just_local(self.local_index, input));
+                    return ControlFlow::Continue(ret);
+                }
+            };
+
+            let ret = handler(Request::Advance(self.step_size, inputs));
+            *self.host_at.as_mut().unwrap() += self.step_size;
+            return ControlFlow::Continue(ret);
+        }
+
+        ControlFlow::Break(None)
+    }
+
+    fn inputs(&self, at: SimulationInstant) -> Option<PlayerInputs> {
+        self.saved_inputs.get(&at).cloned()
+    }
+}
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum Request<'s> {
+    SaveTo(&'s mut SerializedState),
+    Advance(Duration, PlayerInputs),
+    CaptureLocalInput(&'s mut SerializedInput),
+}
+
+#[derive(Debug, Clone)]
+pub struct PlayerInputs<T = SerializedInput> {
+    map: HashMap<PlayerId, T>,
+}
+
+impl PlayerInputs {
+    fn just_local(local_index: PlayerId, input: SerializedInput) -> Self {
+        let mut map = HashMap::default();
+        map.insert(local_index, input);
+        PlayerInputs { map }
+    }
+}
+
+impl<T> PlayerInputs<T> {
+    pub fn map<U>(self, mut f: impl FnMut(T) -> U) -> PlayerInputs<U> {
+        PlayerInputs {
+            map: self.map.into_iter().map(|(k, v)| (k, f(v))).collect(),
+        }
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&PlayerId, &T)> {
+        self.map.iter()
+    }
+}
+
+#[derive(Default)]
+pub struct SessionBuilder {
+    remote_players: Vec<SocketAddr>,
+    local_player: Option<(PlayerId, u16)>,
+    step_size: Option<Duration>,
+}
+
+impl SessionBuilder {
+    pub fn remote_players(&mut self, players: &[SocketAddr]) -> &mut Self {
+        self.remote_players = players.to_vec();
+        self
+    }
+
+    pub fn local_player(&mut self, index: PlayerId, port: u16) -> &mut Self {
+        self.local_player = Some((index, port));
+        self
+    }
+
+    pub fn step_size(&mut self, size: Duration) -> &mut Self {
+        self.step_size = Some(size);
+        self
+    }
+
+    pub fn start(&mut self) -> Result<Session, String> {
+        let (local_index, _port) = self.local_player.ok_or("must provide local_player")?;
+
+        Ok(Session {
+            saved_states: BTreeMap::default(),
+            saved_inputs: BTreeMap::default(),
+            host_at: None,
+            started_at: Instant::now(),
+            step_size: self.step_size.ok_or("must provide step_size")?,
+            local_index,
+        })
+    }
+}
